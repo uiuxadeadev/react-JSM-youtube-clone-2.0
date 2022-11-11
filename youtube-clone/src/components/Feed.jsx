955158bd434dc8c093344fd7d4e9af1fbbec0755
Tai <taiu.engineer@gmail.com>
2022-11-11 22:57:50 +0900

    developed Sidebar menu

--- youtube-clone/src/components/Feed.jsx
+++ youtube-clone/src/components/Feed.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+// import React, { useEffect, useState } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import Sidebar from "./Sidebar";
 
 const Feed = () => {
   return (
-    <div>Feed</div>
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box sx={{ height: { sx: 'auto', md: '92vh'}, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 }}}>
+        <Sidebar />
+        
+        <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: '#fff' }}>
+          Copyright 2022 JSM Media
+        </Typography>
+      </Box>
+
+      <Box>  
+        <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: 'white' }} >
+          New <span style={{ color: '#F31503' }}>videos</span> 
+        </Typography>
+      </Box>
+
+    </Stack>
   )
 }
 
--- youtube-clone/src/components/Navbar.jsx
+++ youtube-clone/src/components/Navbar.jsx
@@ -1,6 +1,8 @@
-import { Stack } from '@mui/material';
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
 
 import { logo } from "../utils/constants";
+import { SearchBar } from "./";
 
 const Navbar = () => (
   <Stack 
