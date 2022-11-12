b8a46631c323f4357b0f8dc5cd0f534963bfa4d0
Tai <taiu.engineer@gmail.com>
2022-11-13 01:46:05 +0900

    develop a function of  Search Feed

--- youtube-clone/src/components/SearchFeed.jsx
+++ youtube-clone/src/components/SearchFeed.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Box, Typography } from "@mui/material";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+import { Videos } from './';
+import { useParams } from "react-router-dom";
 
 const SearchFeed = () => {
+  const [videos, setVideos] = useState([])
+  const { searchTerm } = useParams()
+
+  useEffect(() => {
+    // we need to use ".then", not "const data = "" because it returns a promise with asynchronous functions
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`)
+      .then((data) => setVideos(data.items))
+      .catch((err) => {
+        console.log(err.message)
+       });
+  },[SearchFeed])
+
   return (
-    <div>SearchFeed</div>
+    <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>  
+      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: 'white' }} >
+        Search Results for:
+        <span style={{ color: '#F31503' }}> {searchTerm} </span> 
+        videos
+      </Typography>
+
+      <Videos videos={ videos } />
+    </Box>
   )
 }
 
